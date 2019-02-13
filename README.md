cccda41adb3b577004b8c433eb6581f5460dd1e5
ludekarts <ludekarts@gmail.com>
2019-02-13 09:39:31 +0100

    1.0.0-alpha.1

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # travrs
-An helper library to for creating DOM structures from string templates
+Helper library to for creating DOM structures from string templates
+
+
+## Instalation
+
+```
+npm install -save travrs
+```
+
+
+## API
+
+- **createElement(selector, children?)** (named import) - creates single DOM element.
+- **template(structure)** (default import) - creates DOM structure base on the string template.
+
+
+## Usage
+
+#### Create element syntax
+Travrs uses CSS-like abbreviation syntax for creating DOM nodes.
+```
+  const div = createElement('div#divId.classOne.classTwo[title="My Title" data-hello="world"]', 'This is content');
+```
+
+
+#### Basic
+Travrs uses indentation to detect parent-child relation between noeds.
+
+```
+import template from "travrs";
+
+const element = template(`
+  div.hello
+    h2 > "Hello from Travrs!"  
+`);
+
+document.querySelector("body").appendChild(element);
+
+```
+
+
+#### With references
+Using `@referenceName::` in your template string you can retrive this *tagged* node from **template()** function:
+
+```
+import template from "travrs";
+
+const [element, refs] = template(`
+  div.hello
+    h2 > "Hello from Travrs!"
+    @subtitle::span > "I'm subtitle with reference."
+`);
+
+document.querySelector("body").appendChild(element);
+console.lod(refs.subtitle.textContent);
+
+```
---
+++ index.js
@@ -0,0 +1,90 @@
+// Helper library to for creating DOM structures from string templates.
+// by Wojciech Ludwin, @ludekarts, ludekarts@gmail.com
+
+export const createElement = (phrase, content) => {
+ // Filter attributes.
+ let attrs = phrase.match(/[\w-:]+=".+?"/g);
+ if (attrs) attrs = attrs.map(match => match.replace(/"/g,"").split("="));
+
+ // Filter id, type & classes.
+ const head = ~phrase.indexOf("[") ? phrase.slice(0, phrase.indexOf("[")) : phrase;
+ let id, classes = ~head.indexOf(".") ? head.split(".") : [head], type = classes[0];
+
+ // Separate classes.
+ if (classes.length > 0)
+   classes = classes.slice(1, classes.length).join(" ");
+
+ if (~type.indexOf("#")) [type, id] = type.split("#");
+
+ // Create element.
+ const element = document.createElement(type);
+
+ // Append id.
+ if (id) element.id = id;
+
+ // Append content.
+ if (content) {
+   if (typeof content === "string") {
+     element.innerHTML = content ;
+   }
+   else if (content instanceof HTMLElement) {
+     element.appendChild(content);
+   }
+ }
+
+ // Append attributes.
+ if (attrs) attrs.forEach(attribute => element.setAttribute(attribute[0], attribute[1]));
+ // Append classes.
+ if (classes) element.className = classes;
+
+ return element;
+};
+
+const getIndex = (str) => {
+ const match = str.match(/^\s+/);
+ return match ? match[0].length/2 : 0;
+};
+
+const parentIndex = (array, index, id) => {
+ for (let i = index; i >= 0; i--) {
+   if (getIndex(array[i]) < id) {
+     return i;
+   }
+ }
+ return -1;
+};
+
+
+const create = (id, refs) => {
+ const reference = id.trim().match(/@([\w-_\.]+::)/);
+ id = reference ? id.slice(id.indexOf("::") + 2, id.length) : id;
+ const parentMarker = id.indexOf(">");
+ const type = (parentMarker > -1) ? id.slice(0, parentMarker) : id;
+ const textContent = id.match(/> "(.+)"/);
+ const element = createElement(type.trim(), textContent ? textContent[1] : undefined);
+ reference && (refs[reference[0].slice(1,-2)] = element);
+ return element;
+};
+
+const travrs = (lines) => {
+ const refs = {};
+ const wrapper = create("root");
+ lines
+   .map((el, index) => ({
+       id: index,
+       type: el.trim(),
+       parent: parentIndex(lines, index-1, getIndex(el)),
+       element: create(el, refs)
+     }))
+   .forEach((node, index, lines) =>
+     node.parent === -1
+       ? wrapper.appendChild(node.element)
+       : lines[node.parent].element.appendChild(node.element)
+   );
+
+ return Object.keys(refs).length > 0
+   ? [wrapper.firstChild, refs]
+   : wrapper.firstChild;
+};
+
+export const template = (templateStr) => travrs(templateStr.trim().split("\n"));
